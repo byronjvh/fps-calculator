68327e3c37668dae67d4d23d7e2609deeed9e688
byronjvh <byronjvh02@gmail.com>
2024-11-28 20:43:56 -0600

    Enhance the SelectForm search filter

--- src/components/SelectForm.jsx
+++ src/components/SelectForm.jsx
@@ -3,6 +3,8 @@ import { useState } from 'react'
 export function SelectForm ({ label = 'CPU', array = [], current = 'CPU' }) {
   const [value, setValue] = useState('')
 
+  const filteredArray = array.filter(el => el.name.toLowerCase().includes(value.trim()))
+
   return (
     <div className='w-[300px] select-button'>
       <p>{label}</p>
@@ -11,13 +13,9 @@ export function SelectForm ({ label = 'CPU', array = [], current = 'CPU' }) {
         <input value={value} onChange={(e) => setValue(e.target.value)} className='border border-1 w-full border-slate-400 rounded' type='text' name='' id='' />
         <ul className='pt-2 max-h-[200px] w-full overflow-y-scroll text-xs grid grid-cols-2'>
           {
-            array.filter(el => {
-              console.log(value.lastIndexOf(' '), value.length - 1)
-              return el.name.toLowerCase().includes(value)
-            })
-              .map((el, i) => (
-                <li className='p-1 hover:bg-slate-200 rounded cursor-pointer' key={i}>{el.name}</li>
-              ))
+            filteredArray.map((el, i) => (
+              <li className='p-1 hover:bg-slate-200 rounded cursor-pointer' key={i}>{el.name}</li>
+            ))
           }
         </ul>
       </div>
