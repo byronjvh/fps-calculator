0c528ce50d83464949abe435c4abf230cf350730
byronjvh <byronjvh02@gmail.com>
2024-11-28 23:40:37 -0600

    improve component style and funcionality

--- src/components/SelectForm.jsx
+++ src/components/SelectForm.jsx
@@ -1,16 +1,44 @@
-import { useState } from 'react'
+import { useRef, useState } from 'react'
+import { Arrow } from './Icons'
+import { useClickOutside } from '../hooks/useClickOutside'
+import { searchFilter } from '../logic/filterBySimilarity'
 
-export function SelectForm ({ label = 'CPU', array = [], current = 'CPU' }) {
+export function SelectForm ({ label = 'CPU', array = [], current }) {
   const [value, setValue] = useState('')
+  const [open, setOpen] = useState(false)
+  const optionsRef = useRef()
+  const [randomPlaceholder] = useState(() => {
+    return array[Math.floor(Math.random() * (array.length - 1))].name
+  })
 
-  const filteredArray = array.filter(el => el.name.toLowerCase().includes(value.trim()))
+  const updateOpen = (bol) => {
+    setOpen(bol)
+  }
+
+  useClickOutside(optionsRef, updateOpen)
+
+  const filteredArray = searchFilter(value, array)
 
   return (
-    <div className='w-[300px] select-button'>
+    <div ref={optionsRef} className='w-[300px] select-button'>
       <p>{label}</p>
-      <button className='font-bold w-[300px] border border-1 border-blue-400 p-2 rounded flex'>{current}</button>
-      <div className='p-2 shadow-md rounded'>
-        <input value={value} onChange={(e) => setValue(e.target.value)} className='border border-1 w-full border-slate-400 rounded' type='text' name='' id='' />
+      <button
+        onClick={() => setOpen(prev => !prev)}
+        className='font-medium w-[300px] border border-blue-400 p-2 rounded flex text-orange-600 hover:brightness-110 justify-between'
+      >
+        {current ?? randomPlaceholder}
+        <Arrow className={`fill-orange-500 transition-transform duration-200 ease-out origin-center ${open ? 'rotate-180' : ''}`} />
+      </button>
+      <div className={`p-2 shadow-lg rounded overflow-hidden transition duration-200 origin-top ease-out ${!open ? 'scale-y-0 opacity-0' : 'scale-y-100 opacity-100'}`}>
+        <input
+          placeholder={randomPlaceholder}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className='p-1 text-sm border border-1 w-full border-slate-400 rounded'
+          type='text'
+          name=''
+          id=''
+        />
         <ul className='pt-2 max-h-[200px] w-full overflow-y-scroll text-xs grid grid-cols-2'>
           {
             filteredArray.map((el, i) => (
