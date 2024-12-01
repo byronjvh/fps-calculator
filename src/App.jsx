f89a5cfd106a3551a6cdbb0e33f206c759a4dfd6
byronjvh <byronjvh02@gmail.com>
2024-12-01 16:30:52 -0600

    Added Select component

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,63 @@
+import { useState } from 'react'
 import './App.css'
 import { SelectForm } from './components/SelectForm'
 import AMD_CPU_LIST from './data/cpuAmd.json'
-import INTEL_CPU_LIST from './data/cpuIntel.json'
-import NVIDIA_GPU_LIST from './data/gpuNvidia.json'
+import { Select } from './components/Select'
+// import INTEL_CPU_LIST from './data/cpuIntel.json'
+// import NVIDIA_GPU_LIST from './data/gpuNvidia.json'
 
-const CPU_BRAND = [
-  'AMD',
-  'Intel'
-]
+const CPU_BRAND = {
+  AMD: 'AMD',
+  Intel: 'Intel'
+}
+
+const GPU_BRAND = {
+  AMD: 'AMD',
+  Nvidia: 'Nvidia'
+}
+
+const STORAGE_TYPE = {
+  M2: 'M.2',
+  SATA: 'SATA',
+  HD: 'HD'
+}
+
+const RAM_TYPE = {
+  DDR4: 'DDR4',
+  DDR5: 'DDR5'
+}
+
+const RAM_AMOUNT = {
+  _8GB: '8GB',
+  _16GB: '8GB',
+  _32GB: '8GB',
+  _64GB: '8GB',
+  _128GB: '8GB',
+}
+
+const DEFAULT_CONFIG = {
+  cpuBrand: CPU_BRAND.AMD,
+  gpuBrand: GPU_BRAND.Nvidia,
+  cpu: '',
+  gpu: '',
+  storageType: STORAGE_TYPE.M2,
+  ramType: RAM_TYPE.DDR4,
+  ramAmount: RAM_AMOUNT._32GB
+}
 
 function App () {
+  const [config, setConfig] = useState(DEFAULT_CONFIG)
+  console.log(config)
+
+  const updateCpuBrand = (e) => {
+    setConfig({ ...config, cpuBrand: CPU_BRAND[e.target.value] })
+  }
+
   return (
     <main className='flex flex-col items-center'>
-      <select>
-        {
-        CPU_BRAND.map((el, i) => (
-          <option key={i} value={el.toLowerCase()}>{el}</option>
-        ))
-      }
-      </select>
+      <Select obj={CPU_BRAND} updateState={updateCpuBrand} />
       <div className='flex gap-4'>
         <SelectForm array={AMD_CPU_LIST} />
-        <SelectForm array={INTEL_CPU_LIST} />
-        <SelectForm array={NVIDIA_GPU_LIST} />
       </div>
     </main>
   )
---
+++ src/components/Select.jsx
@@ -0,0 +1,35 @@
+import { useRef, useState } from 'react'
+import { Arrow } from './Icons'
+
+export function Select ({ label = 'CPU Brand', obj, updateState, current }) {
+  const [open, setOpen] = useState(false)
+  const optionsRef = useRef()
+
+  return (
+    <>
+      <div ref={optionsRef} className='w-[300px] select-button'>
+        <p>{label}</p>
+        <button
+          onClick={() => setOpen(prev => !prev)}
+          className='font-medium w-[300px] border border-blue-400 p-2 rounded flex text-orange-600 hover:brightness-110 justify-between'
+        >
+          {current}
+          <Arrow className={`fill-orange-500 transition-transform duration-200 ease-out origin-center ${open ? 'rotate-180' : ''}`} />
+        </button>
+        <div className={`p-2 shadow-lg rounded overflow-hidden transition duration-200 origin-top ease-out ${!open ? 'scale-y-0 opacity-0' : 'scale-y-100 opacity-100'}`}>
+          <ul className='pt-2 max-h-[200px] w-full overflow-y-auto text-xs grid grid-cols-2'>
+            {
+                Object.keys(obj).map((key, i) => (
+                  <li key={i} value={obj[key]}>
+                    <button className='p-1 hover:bg-slate-200 rounded cursor-pointer w-full text-left'>
+                      {obj[key]}
+                    </button>
+                  </li>
+                ))
+            }
+          </ul>
+        </div>
+      </div>
+    </>
+  )
+}
