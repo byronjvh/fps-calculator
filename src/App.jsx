470ef30d6bae9cbc1d9550e475850c209fdcf249
byronjvh <byronjvh02@gmail.com>
2024-12-22 12:36:17 -0600

    added gemini api call to get data

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,11 @@ import INTEL_CPU_LIST from './data/cpuIntel.json'
 import NVIDIA_GPU_LIST from './data/gpuNvidia.json'
 import AMD_GPU_LIST from './data/gpuAmd.json'
 import INTEL_GPU_LIST from './data/gpuIntel.json'
+import GAMES from './data/games.json'
 import { Select } from './components/Select'
 import { CONFIG_OPTIONS, CPU_BRAND, GPU_BRAND, RAM_AMOUNT, RAM_TYPE, STORAGE_TYPE } from './consts'
 import { randomOption } from './logic/randomOption'
+import { getFPSData } from './services/gemini'
 
 const DEFAULT_CONFIG = {
   cpuBrand: CPU_BRAND.AMD,
@@ -22,6 +24,20 @@ const DEFAULT_CONFIG = {
 
 function App () {
   const [config, setConfig] = useState(DEFAULT_CONFIG)
+  const [loading, setLoading] = useState(false)
+
+  const [response, setResponse] = useState('')
+
+  useEffect(() => {
+    getFPSData(config, GAMES)
+      .then((result) => {
+        setResponse(result) // Guarda el texto en el estado
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error)
+      })
+  }, []) // Solo se ejecuta al montar el componente
+  console.log(response)
 
   const updateConfig = (e, name) => {
     const copy = { ...config }
@@ -30,11 +46,14 @@ function App () {
   }
 
   const cpuArrayToUse = config.cpuBrand === CPU_BRAND.AMD ? AMD_CPU_LIST : INTEL_CPU_LIST
-  const gpuArrayToUse = config.gpuBrand === GPU_BRAND.AMD
-    ? AMD_GPU_LIST
-    : config.gpuBrand === GPU_BRAND.Nvidia
-      ? NVIDIA_GPU_LIST
-      : INTEL_GPU_LIST
+  const gpuArrayToUse =
+    config.gpuBrand === GPU_BRAND.AMD
+      ? AMD_GPU_LIST
+      : config.gpuBrand === GPU_BRAND.Nvidia
+        ? NVIDIA_GPU_LIST
+        : config.gpuBrand === GPU_BRAND.Intel
+          ? INTEL_GPU_LIST
+          : []
 
   useEffect(() => {
     setConfig(prev => ({
@@ -44,13 +63,23 @@ function App () {
   }, [config.cpuBrand])
 
   useEffect(() => {
-    setConfig(prev => ({
-      ...prev,
-      gpu: randomOption(gpuArrayToUse)
-    }))
+    setConfig(prev => {
+      if (config.gpuBrand === GPU_BRAND.NO_GRAPHIC_CARD) return { ...prev, gpu: GPU_BRAND.NO_GRAPHIC_CARD }
+      return {
+        ...prev,
+        gpu: randomOption(gpuArrayToUse)
+      }
+    })
   }, [config.gpuBrand])
 
+  const handleClick = () => {
+    setLoading(true)
+    // setTimeout(() => {
+    //   setLoading(false)
+    // }, 3000)
+  }
   console.log(config)
+
   return (
     <main className='w-full max-w-[700px] p-2 pt-24'>
       <h1 className='text-5xl font-bold text-center text-balance mb-2'>Calculate Your PC FPS in Popular Games</h1>
@@ -69,7 +98,12 @@ function App () {
           <Select label='RAM Amount' current={config.ramAmount} name={CONFIG_OPTIONS.ramAmount} obj={RAM_AMOUNT} updateState={updateConfig} />
           <Select label='Storage Type' current={config.storageType} name={CONFIG_OPTIONS.storageType} obj={STORAGE_TYPE} updateState={updateConfig} />
         </div>
-        <button className='flex bg-orange-500 p-2 text-white font-bold justify-center rounded hover:brightness-110 transition duration-200 ease-out'>Calculate</button>
+        <button onClick={() => handleClick()} className='flex relative bg-orange-500 p-2 text-white font-bold justify-center rounded hover:brightness-110 transition duration-200 ease-out'>
+          <span className={`${loading ? 'opacity-0' : ''}`}>Calculate</span>
+          {
+            loading && (<span className='loader absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2' />)
+          }
+        </button>
       </div>
     </main>
   )
